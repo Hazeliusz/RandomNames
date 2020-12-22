aaf8def33637508788ac7cdfd7c61d4711135e81
Hazeliusz <hazeliusz3@gmail.com>
2020-12-22 12:11:27 +0100

    Obsluga bledow i twardosc nazwy Dodano obsluge bledu wpisania wartosci nienumerycznych oraz wyznaczanie twardosci nazwy - czestosci powotrzen spolglosek/samoglosek. 25 = ustawienie domyslne, stosunek 1:1

--- RandomNames/Program.cs
+++ RandomNames/Program.cs
@@ -9,10 +9,44 @@ namespace RandomNames
             char[] vowels = { 'b', 'c', 'ć', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ł', 'm', 'n', 'p', 'r', 's', 't', 'w', 'z', 'ż', 'ź' };
             char[] consonants = { 'a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'y' };
             Random rand = new Random();
-            Console.WriteLine("Podaj długość nazwy: ");
-            int lenght = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj ilość nazw do wygenerowania");
-            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nPodaj długość nazwy: ");
+            int lenght = 1;
+            try
+            {
+                lenght = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Długość nazwy musi być liczbą.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Console.Write("\nPodaj ilość nazw do wygenerowania: ");
+            int amount = 1;
+            try
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ilość musi być liczbą.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Console.Write("\nPodaj \"twardość\" nazwy (1-50): ");
+            int twardosc = 50;
+            try
+            {
+                twardosc = Convert.ToInt32(Console.ReadLine());
+                if (twardosc < 1 || twardosc > 50)
+                    throw new InvalidCastException();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ilość musi być liczbą między 1 a 50.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
             bool rotation = Convert.ToBoolean(rand.Next(0, 1));
             Console.WriteLine();
             for (int j = 0; j < amount; j++)
@@ -20,9 +54,19 @@ namespace RandomNames
                 for (int i = 0; i < lenght; i++)
                 {
                     if (rotation)
-                        Console.Write(vowels[rand.Next(0, vowels.Length - 1)]);
+                    {
+                        if(rand.Next(1 + (50 - twardosc), 100) <= 50 || twardosc == 25)
+                            Console.Write(vowels[rand.Next(0, vowels.Length - 1)]);
+                        else
+                            Console.Write(consonants[rand.Next(0, consonants.Length - 1)]);
+                    }
                     else
-                        Console.Write(consonants[rand.Next(0, consonants.Length - 1)]);
+                    {
+                        if (rand.Next(1 + twardosc, 100) <= 50 || twardosc == 25)
+                            Console.Write(consonants[rand.Next(0, consonants.Length - 1)]);
+                        else
+                            Console.Write(vowels[rand.Next(0, vowels.Length - 1)]);
+                    }
                     rotation = !rotation;
                 }
                 Console.WriteLine();
